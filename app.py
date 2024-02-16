fe064aa2a005ea831631553ecf9810db77c7b82c
Britney <Wu.Britney@gmail.com>
2024-02-16 16:09:11 -0500

    create bootstrap & html files

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/") # routing in views.py
 
-@app.route('/')
-def hello():
-    return "Hi all"
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
